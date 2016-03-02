4bda81d2b5e27a75c7bbf75468d981db176d9b4a
Callum-Sinclair <1101952s@student.gla.ac.uk>
2016-03-02 22:49:21 +0000

    USR: initial work on trying to make a USR packet type for BLE Tx

--- proximity/ble_peripheral/ble_app_cscs/main.c
+++ proximity/ble_peripheral/ble_app_cscs/main.c
@@ -49,6 +49,9 @@
 #include "bsp_btn_ble.h"
 #include "hub_brd.h"
 #include "gpio_lib.h"
+#include "battery_measure_nrf52.h"
+#include "ble_usr.h"
+
 NRF_TIMER_Type* rotation_counter;
 NRF_TIMER_Type* debounce_timer;
 
@@ -104,6 +107,7 @@ NRF_TIMER_Type* debounce_timer;
 #define DEAD_BEEF                       0xDEADBEEF                                 /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
 #define APP_FEATURE_NOT_SUPPORTED       BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2       /**< Reply when unsupported features are requested. */
+#define BAT_PIN                         7 // batery voltage measuring analgoue pin
 
 volatile uint16_t                       adc_sample;
 static uint16_t                         m_conn_handle = BLE_CONN_HANDLE_INVALID;   /**< Handle of the current connection. */
@@ -121,23 +125,12 @@ static sensorsim_state_t                m_crank_rpm_sim_state;                  
 APP_TIMER_DEF(m_battery_timer_id);                                                 /**< Battery timer. */
 APP_TIMER_DEF(m_csc_meas_timer_id);                                                /**< CSC measurement timer. */
 static dm_application_instance_t        m_app_handle;                              /**< Application identifier allocated by device manager. */
-static uint32_t                         m_cumulative_wheel_revs;                   /**< Cumulative wheel revolutions. */
+
 static bool                             m_auto_calibration_in_progress;            /**< Set when an autocalibration is in progress. */
 
-static ble_sensor_location_t supported_locations[] = {BLE_SENSOR_LOCATION_FRONT_WHEEL ,
-                                                      BLE_SENSOR_LOCATION_LEFT_CRANK  ,
-                                                      BLE_SENSOR_LOCATION_RIGHT_CRANK ,
-                                                      BLE_SENSOR_LOCATION_LEFT_PEDAL  ,
-                                                      BLE_SENSOR_LOCATION_RIGHT_PEDAL ,
-                                                      BLE_SENSOR_LOCATION_FRONT_HUB   ,
-                                                      BLE_SENSOR_LOCATION_REAR_DROPOUT,
-                                                      BLE_SENSOR_LOCATION_CHAINSTAY   ,
-                                                      BLE_SENSOR_LOCATION_REAR_WHEEL  ,
-                                                      BLE_SENSOR_LOCATION_REAR_HUB};          /**< supported location for the sensor location. */
+static ble_usr_service_t                     m_xxs;
+static ble_uuid_t m_adv_uuids[] = {{BLE_UUID_USR_SERVICE, BLE_UUID_TYPE_BLE}}; /**< Universally unique service identifiers. */
 
-static ble_uuid_t m_adv_uuids[] = {{BLE_UUID_CYCLING_SPEED_AND_CADENCE,  BLE_UUID_TYPE_BLE},
-                                   {BLE_UUID_BATTERY_SERVICE,            BLE_UUID_TYPE_BLE},
-                                   {BLE_UUID_DEVICE_INFORMATION_SERVICE, BLE_UUID_TYPE_BLE}}; /**< Universally unique service identifiers. */
 
 /**@brief Callback function for asserts in the SoftDevice.
  *
@@ -175,7 +168,7 @@ static void battery_level_update(void)
     uint32_t err_code;
     uint8_t  battery_level;
 
-    battery_level = (uint8_t)usr_measure();
+    battery_level = (uint8_t)battery_measure(BAT_PIN);
 
     err_code = ble_bas_battery_level_update(&m_bas, battery_level);
     if ((err_code != NRF_SUCCESS) &&
@@ -202,49 +195,6 @@ static void battery_level_meas_timeout_handler(void * p_context)
     battery_level_update();
 }
 
-/**@brief Function for populating simulated cycling speed and cadence measurements.
- */
-static void csc_measure(ble_cscs_meas_t * p_measurement)
-{
-    uint32_t result;
-    
-    NRF_SAADC->EVENTS_DONE = 0;
-    NRF_SAADC->TASKS_START = 1;
-    NRF_SAADC->TASKS_SAMPLE = 1;
-    while (NRF_SAADC->EVENTS_DONE == 0);
-    result = adc_sample;
-    
-    static uint16_t cumulative_crank_revs = 0;
-    static uint16_t event_time            = 0;
-    static uint16_t wheel_revolution_mm   = 0;
-    static uint16_t crank_rev_degrees     = 0;
-
-    uint16_t mm_per_sec;
-    uint16_t degrees_per_sec;
-    uint16_t event_time_inc;
-
-    // Per specification event time is in 1/1024th's of a second.
-    event_time_inc = (1024 * SPEED_AND_CADENCE_MEAS_INTERVAL) / 1000;
-
-    // Calculate simulated wheel revolution values.
-    p_measurement->is_wheel_rev_data_present = true;
-    
-    m_cumulative_wheel_revs = rotation_counter->CC[0] / 2; //each pass of the magnet causes two increments
-    wheel_revolution_mm     %= WHEEL_CIRCUMFERENCE_MM;
-
-    p_measurement->cumulative_wheel_revs = 0;
-    p_measurement->last_wheel_event_time = event_time + (event_time_inc);
-
-    // Calculate simulated cadence values.
-    p_measurement->is_crank_rev_data_present = false;
-
-    p_measurement->cumulative_crank_revs = 0;
-    p_measurement->last_crank_event_time = 0;
-    
-    event_time += event_time_inc;
-}
-
-
 /**@brief Function for handling the Cycling Speed and Cadence measurement timer timeouts.
  *
  * @details This function will be called each time the cycling speed and cadence
@@ -255,15 +205,13 @@ static void csc_measure(ble_cscs_meas_t * p_measurement)
  */
 static void csc_meas_timeout_handler(void * p_context)
 {
-    rotation_counter->TASKS_CAPTURE[0] = 1;
     uint32_t        err_code;
-    ble_cscs_meas_t cscs_measurement;
 
     UNUSED_PARAMETER(p_context);
 
-    csc_measure(&cscs_measurement);
+    uint16_t usr_measurement = usr_measure();
 
-    err_code = ble_cscs_measurement_send(&m_cscs, &cscs_measurement);
+    err_code = ble_usr_measurement_send(&m_usr, &usr_measurement);
     if ((err_code != NRF_SUCCESS) &&
         (err_code != NRF_ERROR_INVALID_STATE) &&
         (err_code != BLE_ERROR_NO_TX_PACKETS) &&
@@ -348,21 +296,20 @@ static void gap_params_init(void)
 }
 
 
-/**@brief Function for handling Speed and Cadence Control point events
+/**@brief Function for handling the YYY Service events. 
+ * YOUR_JOB implement a service handler function depending on the event the service you are using can generate
  *
- * @details Function for handling Speed and Cadence Control point events.
- *          This function parses the event and in case the "set cumulative value" event is received,
- *          sets the wheel cumulative value to the received value.
+ * @details This function will be called for all YY Service events which are passed to
+ *          the application.
+ *
+ * @param[in]   p_yy_service   YY Service structure.
+ * @param[in]   p_evt          Event received from the YY Service.
  */
 ble_scpt_response_t sc_ctrlpt_event_handler(ble_sc_ctrlpt_t     * p_sc_ctrlpt,
                                             ble_sc_ctrlpt_evt_t * p_evt)
 {
     switch (p_evt->evt_type)
     {
-        case BLE_SC_CTRLPT_EVT_SET_CUMUL_VALUE:
-            m_cumulative_wheel_revs = p_evt->params.cumulative_value;
-            break;
-
         case BLE_SC_CTRLPT_EVT_START_CALIBRATION:
             m_auto_calibration_in_progress = true;
             break;
@@ -382,30 +329,29 @@ ble_scpt_response_t sc_ctrlpt_event_handler(ble_sc_ctrlpt_t     * p_sc_ctrlpt,
 static void services_init(void)
 {
     uint32_t              err_code;
-    ble_cscs_init_t       cscs_init;
+    ble_cscs_init_t        usr_init;
     ble_bas_init_t        bas_init;
     ble_dis_init_t        dis_init;
-    ble_sensor_location_t sensor_location;
 
     // Initialize Cycling Speed and Cadence Service.
-    memset(&cscs_init, 0, sizeof(cscs_init));
+    memset(&usr_init, 0, sizeof(usr_init));
 
-    cscs_init.evt_handler = NULL;
-    cscs_init.feature     = BLE_CSCS_FEATURE_WHEEL_REV_BIT | BLE_CSCS_FEATURE_CRANK_REV_BIT |
+    usr_init.evt_handler = NULL;
+    usr_init.feature     = BLE_CSCS_FEATURE_WHEEL_REV_BIT | BLE_CSCS_FEATURE_CRANK_REV_BIT |
                             BLE_CSCS_FEATURE_MULTIPLE_SENSORS_BIT;
 
     // Here the sec level for the Cycling Speed and Cadence Service can be changed/increased.
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cscs_init.csc_meas_attr_md.cccd_write_perm);   // for the measurement characteristic, only the CCCD write permission can be set by the application, others are mandated by service specification
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cscs_init.csc_feature_attr_md.read_perm);      // for the feature characteristic, only the read permission can be set by the application, others are mandated by service specification
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cscs_init.csc_ctrlpt_attr_md.write_perm);      // for the SC control point characteristic, only the write permission and CCCD write can be set by the application, others are mandated by service specification
-    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cscs_init.csc_ctrlpt_attr_md.cccd_write_perm); // for the SC control point characteristic, only the write permission and CCCD write can be set by the application, others are mandated by service specification
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&usr_init.csc_meas_attr_md.cccd_write_perm);   // for the measurement characteristic, only the CCCD write permission can be set by the application, others are mandated by service specification
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&usr_init.csc_feature_attr_md.read_perm);      // for the feature characteristic, only the read permission can be set by the application, others are mandated by service specification
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&usr_init.csc_ctrlpt_attr_md.write_perm);      // for the SC control point characteristic, only the write permission and CCCD write can be set by the application, others are mandated by service specification
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&usr_init.csc_ctrlpt_attr_md.cccd_write_perm); // for the SC control point characteristic, only the write permission and CCCD write can be set by the application, others are mandated by service specification
     
-    cscs_init.ctrplt_supported_functions    = BLE_SRV_SC_CTRLPT_CUM_VAL_OP_SUPPORTED
+    usr_init.ctrplt_supported_functions    = BLE_SRV_SC_CTRLPT_CUM_VAL_OP_SUPPORTED
                                               |BLE_SRV_SC_CTRLPT_SENSOR_LOCATIONS_OP_SUPPORTED
                                               |BLE_SRV_SC_CTRLPT_START_CALIB_OP_SUPPORTED;
-    cscs_init.ctrlpt_evt_handler            = sc_ctrlpt_event_handler;
-    cscs_init.list_supported_locations      = supported_locations;
-    cscs_init.size_list_supported_locations = sizeof(supported_locations) / sizeof(ble_sensor_location_t);            
+    usr_init.ctrlpt_evt_handler            = sc_ctrlpt_event_handler;
+    usr_init.list_supported_locations      = supported_locations;
+    usr_init.size_list_supported_locations = sizeof(supported_locations) / sizeof(ble_sensor_location_t);            
     
     sensor_location           = BLE_SENSOR_LOCATION_FRONT_WHEEL;                    // initializes the sensor location to add the sensor location characteristic.
     cscs_init.sensor_location = &sensor_location;
