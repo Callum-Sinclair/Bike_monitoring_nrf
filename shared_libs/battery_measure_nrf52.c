039ec76e100131ac26cf4164cbc2a74739ebb012
Callum-Sinclair <1101952s@student.gla.ac.uk>
2016-03-02 18:22:33 +0000

    BATTERY: shared lib now set up to use internal reference and return valid battery% - UNTESTED

--- shared_libs/battery_measure_nrf52.c
+++ shared_libs/battery_measure_nrf52.c
@@ -8,12 +8,12 @@ void adc_init(uint8_t a_pin)
     NRF_SAADC->RESOLUTION = SAADC_RESOLUTION_VAL_8bit;
     NRF_SAADC->SAMPLERATE = SAADC_SAMPLERATE_MODE_Task << SAADC_SAMPLERATE_MODE_Pos;
     NRF_SAADC->RESULT.MAXCNT = 1;
-    NRF_SAADC->CH[0].CONFIG = ((SAADC_CH_CONFIG_RESP_Bypass   << SAADC_CH_CONFIG_RESP_Pos) | \
-                               (SAADC_CH_CONFIG_RESN_Bypass   << SAADC_CH_CONFIG_RESN_Pos) | \
-                               (SAADC_CH_CONFIG_GAIN_Gain1_4  << SAADC_CH_CONFIG_GAIN_Pos) | \
-                               (SAADC_CH_CONFIG_REFSEL_VDD1_4 << SAADC_CH_CONFIG_REFSEL_Pos) | \
-                               (SAADC_CH_CONFIG_TACQ_10us     << SAADC_CH_CONFIG_TACQ_Pos) | \
-                               (SAADC_CH_CONFIG_MODE_SE       << SAADC_CH_CONFIG_MODE_Pos));
+    NRF_SAADC->CH[0].CONFIG = ((SAADC_CH_CONFIG_RESP_Bypass     << SAADC_CH_CONFIG_RESP_Pos) | \
+                               (SAADC_CH_CONFIG_RESN_Bypass     << SAADC_CH_CONFIG_RESN_Pos) | \
+                               (SAADC_CH_CONFIG_GAIN_Gain1_5    << SAADC_CH_CONFIG_GAIN_Pos) | \
+                               (SAADC_CH_CONFIG_REFSEL_Internal << SAADC_CH_CONFIG_REFSEL_Pos) | \
+                               (SAADC_CH_CONFIG_TACQ_10us       << SAADC_CH_CONFIG_TACQ_Pos) | \
+                               (SAADC_CH_CONFIG_MODE_SE         << SAADC_CH_CONFIG_MODE_Pos)); //gives Vref 0.6 V, but input gain 1/5, so 100% input = 3V
 
     NRF_SAADC->ENABLE = 1;    
 }
@@ -27,7 +27,11 @@ uint8_t battery_measure(uint8_t a_pin)
     NRF_SAADC->TASKS_START = 1;
     NRF_SAADC->TASKS_SAMPLE = 1;
     while (NRF_SAADC->EVENTS_DONE == 0);
-    //uint32_t result = adc_sample;
+    // Max voltage (3V) = 100 % = 255 = 0xFF (8bit)
+    // Min voltage (1.2V) = 0 % = 102 = 0x66
+    // Range = 153
     
-    return (uint8_t)result;
+    uint8_t bat_percent = (uint8_t)(((adc_sample - BAT_0) * 100) / (BAT_100 - BAT_0)); // calcutate the battery percentage
+    
+    return bat_percent;
 }
--- shared_libs/battery_measure_nrf52.h
+++ shared_libs/battery_measure_nrf52.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "nrf.h"
+#define BAT_100 255
+#define BAT_0   102
 
 void adc_init(uint8_t a_pin);
 uint8_t battery_measure(uint8_t a_pin);
