c6654ffdad56df70f9149c12ddd07cdbd249131c
Callum-Sinclair <1101952s@student.gla.ac.uk>
2016-05-12 14:46:58 +0100

    project files changed on close

--- hub/ble_central_and_peripheral/experimental/ble_app_hrs_rscs_relay/pca10040/s132/arm5/RTE/RTE_Components.h
+++ hub/ble_central_and_peripheral/experimental/ble_app_hrs_rscs_relay/pca10040/s132/arm5/RTE/RTE_Components.h
@@ -4,7 +4,7 @@
  *      *** Do not modify ! ***
  *
  * Project: 'ble_app_hrs_rscs_relay_pca10040' 
- * Target:  'flash_s132_nrf52_2.0.0-7.alpha_softdevice' 
+ * Target:  'nrf52832_xxaa_s132' 
  */
 
 #ifndef RTE_COMPONENTS_H
