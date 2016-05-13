58e97a2998f694dedb0aeac4ba8a17db63737de6
Callum-Sinclair <1101952s@student.gla.ac.uk>
2016-05-13 11:49:34 +0100

    files added automatically for allowing Tester to run on nrf52

---
+++ tester/ble_peripheral/ble_app_template/pca10040/s132/arm5/RTE/RTE_Components.h
@@ -0,0 +1,28 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_template_s132_pca10040' 
+ * Target:  'nrf52832_xxaa_s132' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define NRF_LOG_USES_UART=1
+#define S132
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */
